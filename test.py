ba7e47e55bcf07792947d1d3ebb00a132dbed7b1
Darkz <166989020+darkz0823@users.noreply.github.com>
2024-09-03 15:21:59 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-import pyautogui
-import random
-import time
+import ctypes
+import requests
+from io import BytesIO
+from PIL import Image
 
-duration = 10
-end_time = time.time() + duration
+def change_wallpaper_from_url(image_url):
+    try:
+        response = requests.get(image_url)
+        response.raise_for_status()
 
-while time.time() < end_time:
-    screen_width, screen_height = pyautogui.size()
+        image = Image.open(BytesIO(response.content))
+        bmp_path = "temp_wallpaper.bmp"
+        image.save(bmp_path, "BMP")
 
-    x = random.randint(0, screen_width - 1)
-    y = random.randint(0, screen_height - 1)
-
-    pyautogui.moveTo(x, y, duration=0.1)
-
-    time.sleep(0.01)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, bmp_path, 0)
+        return "Hình nền đã được thay đổi!"
+    except Exception as e:
+        return f"Đã xảy ra lỗi: {str(e)}"
